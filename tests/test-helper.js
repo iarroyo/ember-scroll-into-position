ea972024ec49cebb72a40c56a0ad3a5642ada69c
Ivan <iarroyo@copyright.com>
2022-06-09 16:31:23 +0200

    Initial commit

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'ember-scroll-into-position/app';
+import config from 'ember-scroll-into-position/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
